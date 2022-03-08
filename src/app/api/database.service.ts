8b2a2b50b5ab7e54d2d5929f04b2f88409e4bb9f
EkizBurak <86228168+EkizBurak@users.noreply.github.com>
2022-03-08 23:02:09 +0300

    Add files via upload

--- src/app/api/database.service.ts
+++ src/app/api/database.service.ts
@@ -1,92 +1,80 @@
 import { Injectable } from '@angular/core';
-import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
+import { SQLite, SQLiteObject } from '@awesome-cordova-plugins/sqlite/ngx';
+import { Platform } from '@ionic/angular';
 import { Toast } from '@ionic-native/toast/ngx';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DatabaseService {
   databaseNesnesi: SQLiteObject;
-  constructor(private sqlite: SQLite, private toast: Toast) {}
-  /*
-  getUserCount() {
-    this.databaseNesnesi
-      .executeSql('select count(*) from usersdata', [])
-      .then((data) => {
-        this.sendMsg(JSON.stringify(data));
-        return data;
-      })
-      .catch(() => {
-        return 0;
-      });
-  }
-  */
-  createUser(firstName, lastName, age, gender, musicPlatform, language) {
-    this.sqlite
-      .create({
-        name: 'users.db',
-        location: 'default',
-      })
-      .then((db: SQLiteObject) => {
-        db.executeSql(
-          'create table IF NOT EXISTS usersdata(firstName TEXT, lastName TEXT, age INTEGER, gender TEXT, musicPlatform TEXT, language TEXT)',
-          []
-        );
-        this.databaseNesnesi = db;
-      })
-      .catch((e) => this.sendMsg(JSON.stringify(e)));
+  userCount: number;
+  constructor(
+    private sqlite: SQLite,
+    private toast: Toast,
+    public platform: Platform,
+    private router: Router
+  ) {
+    platform.ready().then(() => {
+      this.sqlite
+        .create({
+          name: 'users.db',
+          location: 'default',
+        })
+        .then((db: SQLiteObject) => {
+          db.executeSql(
+            'create table IF NOT EXISTS usersdata(firstName TEXT, lastName TEXT, age INTEGER, gender TEXT, musicPlatform TEXT, language TEXT)',
+            []
+          );
 
-    this.databaseNesnesi
-      .executeSql(
-        `insert into usersdata (title,desc) values ('${firstName}','${lastName}','${age}','${gender}','${firstName}','${musicPlatform}','${language}')`
-      )
-      .then((data) => {
-        this.sendMsg('Veri Eklendi');
-      });
+          this.databaseNesnesi = db;
+        })
+        .then(() => {
+          this.getUserCount().then((result) => {
+            this.userCount = result.rows.length;
+
+            if (this.userCount >= 1) {
+              this.router.navigateByUrl('/better-than-stylist');
+            }
+          });
+        })
+        .catch((e) => this.sendMsg(JSON.stringify(e)));
+    });
   }
   sendMsg(msg) {
     this.toast.show(msg, '5000', 'bottom').subscribe((toast) => {
       console.log(toast);
     });
   }
-  /* 
-  async createDataBase() {
-    this.sendMsg('sad');
-    await this.sqlite
-      .create({ name: 'todo', location: 'default' })
-      .then((db: SQLiteObject) => {
-        this.databaseNesnesi = db;
-      })
-      .catch((e) => {
-        alert('error on creating database' + JSON.stringify(e));
-      });
-    await this.createDataBase();
-  }
-  
-  async createTables() {
-    await this.databaseNesnesi.executeSql(
-      'CREATE TABLE IF NOT EXITS todo(ID INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, desc TEXT)',
-      []
-    );
-  }
-  async addCategory(as3: string, as2: string) {
+  getUserCount() {
     return this.databaseNesnesi
-      .executeSql(
-        `insert into todo (title,desc) values ('${as3}','${as2}')`,
-        []
-      )
-      .then(() => {
-        this.sendMsg('category created');
-        return 'category created';
-      })
-      .catch((e) => {
-        if (e.code === 6) {
-          this.sendMsg('category already exists');
-          return 'category already exists';
-        }
-        this.sendMsg('error on creating category ');
-        return 'error on creating category ' + JSON.stringify(e);
+      .executeSql('select * from usersdata', [])
+      .then((data) => {
+        return data;
       });
   }
-  */
+
+  createUser(firstName, lastName, age, gender, musicPlatform, language) {
+    if (this.userCount >= 1) {
+      this.sendMsg('User already exit');
+    } else {
+      this.databaseNesnesi.executeSql(
+        `insert into usersdata (firstName, lastName, age, gender, musicPlatform, language) values ('${firstName}','${lastName}','${age}','${gender}','${musicPlatform}','${language}')`
+      );
+
+      this.sendMsg('User Create Successfully');
+
+      this.userCount += 1;
+    }
+    this.router.navigateByUrl('/better-than-stylist');
+  }
+  updateUsers(firstName, lastName, age, gender, musicPlatform, language) {
+    // Update İşlemi
+    this.databaseNesnesi.executeSql(
+      `update usersdata set firstName = '${firstName}', lastName= '${lastName}', age= '${age}', gender= '${gender}', musicPlatform= '${musicPlatform}', language= '${language}'`
+    );
+
+    this.sendMsg('User update successfully');
+  }
 }
