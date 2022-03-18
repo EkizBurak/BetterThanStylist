543c52d0059c473a469a1e5680dba9f8deb983c4
EkizBurak <86228168+EkizBurak@users.noreply.github.com>
2022-03-18 09:17:56 +0300

    Add files via upload

---
+++ src/app/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./home/home.module').then((m) => m.HomePageModule),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'better-than-stylist',
+    loadChildren: () =>
+      import('./better-than-stylist/better-than-stylist.module').then(
+        (m) => m.BetterThanStylistPageModule
+      ),
+  },
+
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
