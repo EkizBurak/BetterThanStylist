aea65738d6a2a59e700a61b25ef3f5a24c9b7712
EkizBurak <86228168+EkizBurak@users.noreply.github.com>
2022-03-27 03:30:08 +0300

    Create app.module.ts

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DatabaseService } from '../app/api/database.service';
+import { FormsModule } from '@angular/forms';
+import { SQLite, SQLiteObject } from '@awesome-cordova-plugins/sqlite/ngx';
+import { Toast } from '@ionic-native/toast/ngx';
+import { HttpClientModule,HttpClient } from '@angular/common/http';
+import { Geolocation } from '@awesome-cordova-plugins/geolocation/ngx';
+import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
+import { HTTP } from '@awesome-cordova-plugins/http/ngx';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+
+  imports: [
+    FormsModule,
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule,
+  ],
+  providers: [
+    DatabaseService,
+    SQLite,
+    Toast,
+    Geolocation,
+    AndroidPermissions,
+    HTTP,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
